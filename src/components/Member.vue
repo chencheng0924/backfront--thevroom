399bdbe98cd2b9452e290be49e209d71c3551726
schiho47 <s.chiho47@gmail.com>
2021-08-18 18:03:37 +0800

    檢舉留言差一個按鈕

--- src/components/Member.vue
+++ src/components/Member.vue
@@ -2,44 +2,31 @@
     <div>
         <div class="flex justify-between m-10">
             <div class="text-2xl">會員資料管理</div>
+        </div>
+        <div>
             <dialoggo :show="dialogs">
-                <div class="flex flex-col w-full justify-around">
-                    <div class="w-full mb-5">
-                      <div class="text-2xl text-black text-center">---會員資料---</div>
-                    </div>
-                    <div class="flex justify-between">
-                      <uploadimage class="mr-3" />
-                      <dialig-car-date @gogoro='close'/>
-                    </div>
-                </div>
+                <member-data @gogoro='close' :userid="userid"/>
             </dialoggo>
         </div>
         <div class="flex flex-col items-center">
             <div class="w-11/12 flex flex-col justify-center">
                 <div class="flex text-xl w-full justify-around items-center mb-3">
-                    <div class="w-[60px]">會員ID</div>
-                    <div class="w-[60px]">姓名</div>
-                    <div class="w-[60px]">性別</div>
-                    <div class="w-[60px]">生日</div>
-                    <div class="w-[150px]">電子信箱</div>
-                    <div class="w-[60px]">狀態</div>
-                    <div class="w-[60px]"><button>檢視</button></div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">會員ID</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">姓名</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">生日</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">電子信箱</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">狀態</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center"><button>檢視</button></div>
                 </div>
                 <div class="bg-black h-[2px]"></div>
-                <div class="w-full flex text-lg justify-around items-center my-5" v-for="man in member" :key="man.id">
-                    <div class="w-[60px]">{{man.id}}</div>
-                    <div class="w-[60px]">{{man.name}}</div>
-                    <div class="w-[60px]">{{man.gender}}</div>
-                    <div class="w-[60px]">{{man.bir}}</div>
-                    <div class="w-[150px]">{{man.email}}</div>
-                    <div class="w-[60px]">已啟用</div>
-                    <div class="w-[60px]"><button @click="gogo" class="px-2 py-3 rounded-lg bg-black text-white">編輯</button></div>
+                <div class="w-full flex text-lg justify-around items-center my-5" v-for="member in members" :key="member.id">
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">{{member[0]}}</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">{{member[4]}}</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">{{member[5]}}</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">{{member[2]}}</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center">已啟用</div>
+                    <div style="border: 1px solid red" class="w-11/12 flex justify-center"><button @click="gogo(member[0])" class="w-[80px] h-[40px] rounded-lg bg-black text-white">編輯</button></div>
                 </div>
-            </div>
-            <div>
-                <dialoggo :show="dialogs">
-                    <member-data @gogoro='close'/>
-                </dialoggo>
             </div>
         </div>
     </div>
@@ -51,19 +38,38 @@ export default {
     components:{
         MemberData
     },
+    async created () {
+        const res = await fetch('http://localhost:8080/backfront-php/SelectMemberData.php')
+        const resdata = await res.json()
+        console.log(res)
+        console.log(resdata)
+        this.members = [...resdata]
+        // this.forums = this.forums1.push(resdata)
+        console.log(this.members)
+    },
     data() {
         return {
             dialogs: false,
-            member:[
-                {id:'001',name:'tony',gender:'1',bir:'1992/9/24',email:'abs255667@yahoo.com.tw'},
-                {id:'002',name:'allen',gender:'1',bir:'1993/2/5',email:'abs255667@yahoo.com.tw'},
-                {id:'003',name:'jessie',gender:'0',bir:'1996/3/9',email:'abs255667@yahoo.com.tw'},
-            ]
+            // members:[
+            //     {id:'001',name:'tony',gender:'1',bir:'1992/9/24',email:'abs255667@yahoo.com'},
+            //     {id:'002',name:'allen',gender:'1',bir:'1993/2/5',email:'abs255667@yahoo.com'},
+            //     {id:'003',name:'jessie',gender:'0',bir:'1996/3/9',email:'abs255667@yahoo.com'},
+            // ]
+            members: [],
+            userid: ''
         }
     },
     methods:{
-        gogo(){
+        gogo(a){
             this.dialogs = true
+            this.userid = a
+            console.log(this.userid)
+            // const fd = new FormData()
+            // fd.append('artID', a)
+            // fetch('http://localhost:8080/phpfile/SelectDiologArticle.php', {
+            //     method: 'POST',
+            //     body: fd
+            // })
         },
         close(){
             this.dialogs = false
--- src/components/dialog/MemberData.vue
+++ src/components/dialog/MemberData.vue
@@ -1,61 +1,112 @@
 <template>
-    <form style="min-width:900px">
+    <form style="min-width:400px">
         <div class="w-full h-[50px] text-black text-2xl text-center">
             <h1>---會員資料---</h1>
         </div>
-        <div class="flex w-full justify-around text-lg mb-5">
+
+        <div class="flex w-full justify-center text-lg mb-5">
             <div class="flex flex-col max-w-[470px]">
                 <div class="w-[120px]">會員ID:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="one">
             </div>
+        </div>
+
+        <div class="flex w-full justify-center text-lg mb-5">
             <div class="flex flex-col max-w-[470px]">
                 <div class="w-[120px]">密碼:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="two">
             </div>
         </div>
-        <div class="flex w-full justify-around text-lg mb-5">
+
+        <div class="flex w-full justify-center text-lg mb-5">
             <div class="flex flex-col max-w-[470px]">
                 <div class="w-[120px]">姓名:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
-            </div>
-            <div class="flex flex-col max-w-[470px]">
-                <div class="w-[120px]">性別:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="three">
             </div>
         </div>
-        <div class="flex w-full justify-around text-lg mb-8">
+
+        <div class="flex w-full justify-center text-lg mb-5">
             <div class="flex flex-col max-w-[470px]">
                 <div class="w-[120px]">電子信箱:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="four">
             </div>
+        </div>
+
+        <div class="flex w-full justify-center text-lg mb-5">
             <div class="flex flex-col max-w-[470px]">
                 <div class="w-[120px]">生日:</div>
-                <input type="text" class="border-black border rounded-md w-[350px]">
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="five">
             </div>
         </div>
-        <div class="flex w-full justify-center text-lg mb-5">
-            <div class="flex max-w-[800px]">
-                <div class="w-[80px]">地址:</div>
-                <input type="text" class="border-black border rounded-md w-[720px]">
+
+        <div class="flex w-full justify-center text-lg mb-8">
+            <div class="flex flex-col max-w-[470px]">
+                <div class="w-[120px]">地址:</div>
+                <input type="text" class="border-black border rounded-md w-[350px]" v-model="six">
             </div>   
         </div>
-        <div class="flex w-full justify-end text-lg mb-3">
+        <div class="flex w-full justify-center text-lg mb-5">
             <button @click.prevent="gogoro2" class="border px-3 py-1">取消</button>
-            <div class="bg-blue-600 ml-5"><button class="px-3 py-1 text-white">確認</button></div>
+            <div class="bg-blue-600 ml-5"><button @click.prevent="updatebtn" class="px-3 py-1 text-white">確認</button></div>
         </div>
     </form>
 </template>
 
 <script>
 export default {
+    props: ['userid'],
+    async created () {
+        const fd = new FormData()
+        fd.append('userid', this.userid)
+        const res = await fetch('http://localhost:8080/backfront-php/SelectDialogMember.php', {
+            method: 'POST',
+            body: fd
+        });
+        const resdata = await res.json();
+        this.user = resdata
+        console.log(this.user[0]);
+        this.one = this.user[0][0];
+        this.two = this.user[0][1];
+        this.three = this.user[0][4];
+        this.four = this.user[0][2];
+        this.five = this.user[0][5];
+        this.six = this.user[0][3];
+        console.log(this.userid);
+    },
     data() {
         return {
             close: false,
+            user: [],
+            one: '', 
+            two: '',
+            three: '',
+            four: '',
+            five: '',
+            six: ''
         }
     },
     methods:{
         gogoro2(){
             this.$emit('gogoro', this.close)
+        },
+        updatebtn(){
+            console.log(this.one);
+            console.log(this.two);
+            console.log(this.three);
+            console.log(this.four);
+            console.log(this.five);
+            console.log(this.six);
+            const formdata = new FormData();
+            formdata.append('PASSWORD', this.two)
+            formdata.append('EMAIL', this.four)
+            formdata.append('ADDRESS', this.six)
+            formdata.append('FULLNAME', this.three)
+            formdata.append('BIRTHDAY', this.five)
+            formdata.append('MEMBERID', this.one)
+            fetch('http://localhost:8080/backfront-php/UpdateDialogMember.php', {
+                method: 'POST',
+                body: formdata
+            })
         }
     }
 }
